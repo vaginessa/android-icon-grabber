554f4566ed7680e181c77ea4bc1de21a41d481b3
John Carlson <carlson@ludomade.com>
2015-05-21 13:32:26 -0500

    Move javascript into its own file and respond to enter pressed on input. Remove unused post-service html

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-(function() {
-    'use strict';
+var button = document.getElementById("search_button");
 
-    /* Polymer UI and UX */
-
-console.log("loading template");
-    var template = document.querySelector('template[is=auto-binding]');
+  var template = document.querySelector('template[is=auto-binding]');
 
     template.checkKey = function(e) {
-    	console.log("Enter pressed");
         if(e.keyCode === 13 || e.charCode === 13) {
-            console.log("Enter pressed");
+          button.click();
         }
-    };
+  };
 
-})();
+  function getSearchTerm() {
+  
+    console.log("getSearchTerm");
+    //start spinner
+    var spinner = document.getElementById("spinner");
+    spinner.active = true;
+    var SEARCH_URL = "https://play.google.com/store/search?q=";
+    var APP_URL = "https://play.google.com/store/apps/details?id=";
+    var search_field = document.getElementById("search_term");
+    var searchTerm = document.getElementById("search_term").value;
+    var autoDownload = document.getElementById("autodownload_check").checked;
+    //Get the value of the input
+  
+  
+  console.log("calling php");
+    $.ajax({
+          url:'search_for_app.php',
+          data: { query: searchTerm},
+          type: "GET",
+          complete: function (response) {
+            console.log(response.responseText);
+            if (response.responseText == "error") {
+              console.log("server returned error");
+              $("#demo_img").attr('src', "/images/ic_alert_warning");
+            } else { 
+              $("#demo_img").attr('src', response.responseText);
+              $("#image_link").attr('href', response.responseText);
+            }
+            spinner.active = false;
+            if (autoDownload) {
+              $("#demo_img").click();
+            }
+            //TODO autodownload images?
+            //var url = img.src.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+            //window.open(url);
+          },
+          error: function () {
+            alert("Could not download icon.");
+            spinner.active = false;
+          }
+    });
+     
+  }
